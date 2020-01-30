ad9b0edbb632866b0b134dd06564acfe750a769a
Wesley Webster <wesleywebster91@gmail.com>
2020-01-30 08:20:37 -0600

    Added functionality
    Now able to add items to the burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BuildControls.module.css';
 
 import BuildControl from './BuildControl/BuildControl';
- 
+
 /*Macro, in charge of the console of controls
 - Our controls is an array that contains object that we will pass off as property inside our
 BuildControl component. 
@@ -10,7 +10,7 @@ BuildControl component.
 to add more labels simply edit the array and add a new object. It is rendered dynamically.
 */
 const controls = [
-    { label: 'Salad', type: 'salad' },
+    { label: 'Lettuce', type: 'lettuce' },
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
     { label: 'Meat', type: 'meat' },
@@ -24,7 +24,10 @@ the BuildControl is receiving props.
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} />
         ))}
     </div>
 );
