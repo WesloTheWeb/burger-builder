cbafc757663aa54efca27fa8484b69bb67f4bae8
Wesley Webster <wesleywebster91@gmail.com>
2020-02-03 08:36:16 -0600

    Adding in mobile responsiveness
    Starting to add mobile menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import classes from './layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Auxillary>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Layout}>
             {props.children}
         </main>
