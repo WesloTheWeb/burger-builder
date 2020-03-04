007272724eb07ecb3cd9c3d087dbc562bb2e5ff8
Wesley Webster <wesleywebster91@gmail.com>
2020-03-04 01:20:00 -0600

    back to it
    Setting up to add routing after a 2.5 week hiatus.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
- 
+import Checkout from './containers/Checkout/Checkout';
+
 class App extends Component {
   render() {
-    return  (
+    return (
       <Layout>
-        <BurgerBuilder/>
-       </Layout>
+        <BurgerBuilder />
+        <Checkout />
+      </Layout>
     );
-  } 
+  }
 }
 
 export default App;
