619f96d209dd63096212907e0dd434fedebe716b
Wesley Webster <wesleywebster91@gmail.com>
2020-03-05 23:41:43 -0600

    Orders page completed
    Still have a bug but orders page is completed.
    Currently cannot see the ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Wesley Webster',
-        //         address: {
-        //             street: 'Sesame Street 123',
-        //             zipCode: '12345',
-        //             country: 'Germany'
-        //         },
-        //         email: 'uwu@owo.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
+   
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
